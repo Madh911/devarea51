9e6b3dda46bc7e0b13cbf47178828980a7269f34
Madhu Shankara G S <98076468+Madh911@users.noreply.github.com>
2023-10-26 00:33:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const passwordForm = document.getElementById("password-form");
     const passwordInput = document.getElementById("password");
     const message = document.getElementById("message");
-    const nextPage = document.getElementById("next-page");
+    const secretContent = document.getElementById("secret-content");
+    const copyButton = document.getElementById("copy-button");
 
     passwordForm.addEventListener("submit", function(event) {
         event.preventDefault();
         const enteredPassword = passwordInput.value.trim();
 
-      
         if (enteredPassword === "youknow") {
-            // Show the next page and hide the password entry page
-            nextPage.classList.remove("hidden");
+            secretContent.classList.remove("hidden");
             passwordForm.style.display = "none";
         } else {
-          
             message.textContent = "Incorrect password. Please try again.";
             passwordInput.value = "";
         }
+    });
+
+    copyButton.addEventListener("click", function() {
+        const codeToCopy = document.querySelector("pre");
+        const tempInput = document.createElement("input");
+        tempInput.value = codeToCopy.textContent;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+        alert("Code copied to clipboard!");
     });
 });
